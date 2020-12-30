d03f9bd6849d8cf6a4d5f8cb2c106729160b0d98
Łukasz Starzec <luk.starzec@gmail.com>
2020-12-30 08:48:10 +0100

    Initial version

---
+++ ColorBook/ColorBook/Scripts/clipboardCopy.js
@@ -0,0 +1,14 @@
+﻿
+window.clipboardCopy = {
+    copyText: function copyText(text) {
+        return navigator
+            .clipboard.writeText(text)
+            .then(function () {
+                return true;
+            })
+            .catch(function (error) {
+                console.error(error);
+                return false;
+            });
+    }
+};
---
+++ ColorBook/ColorBook/wwwroot/scripts/clipboardCopy.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+window.clipboardCopy = {
+    copyText: function copyText(text) {
+        return navigator.clipboard.writeText(text).then(function () {
+            return true;
+        })["catch"](function (error) {
+            console.error(error);
+            return false;
+        });
+    }
+};
+
