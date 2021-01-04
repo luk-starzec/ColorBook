ea316a78208dd3d6c68a174f2c5ac06944101206
Łukasz Starzec <luk.starzec@gmail.com>
2021-01-04 21:26:21 +0100

    Dodana obsługa paczek npm. Dodany npm color-js

---
+++ ColorBook/ColorBook/JsLib/src/index.js
@@ -0,0 +1,9 @@
+﻿import Color from 'color-js';
+
+export function colorTest() {
+    var green = Color("#00FF00");
+    console.log(green);
+    console.log(green.toHSV())
+    console.log(green.getHue())
+    return green.toString();
+}
