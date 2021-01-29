382b98bfd9c152435b86dafcfeb354569d066aea
Łukasz Starzec <luk.starzec@gmail.com>
2021-01-29 17:53:18 +0100

    Dodany komponent ColorSelection Dodany komponent ColorsScheme

---
+++ ColorBook/ColorBook/Models/EnumColorSelectionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColorBook.Models
+{
+    public enum EnumColorSelectionType
+    {
+        Palette,
+        Image,
+        Camera
+    }
+}
