3089fcb4f7a4febef5dea003fa82d5ad2e39e466
Łukasz Starzec <luk.starzec@gmail.com>
2021-02-04 20:35:42 +0100

    Testowy eksport/import plików

---
+++ ColorBook/ColorBook/Models/ColorScheme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColorBook.Models
+{
+    public class ColorScheme
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public KeyValuePair<string, string>[] Colors { get; set; }
+    }
+}
