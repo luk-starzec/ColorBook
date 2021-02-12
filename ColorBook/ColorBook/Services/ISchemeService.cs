567cf070de6c4553569d3ae54190e43d1402da8b
Łukasz Starzec <luk.starzec@gmail.com>
2021-02-12 20:31:29 +0100

    Wstępna wersja ładowania schematów

--- ColorBook/ColorBook/Services/ISchemeService.cs
+++ ColorBook/ColorBook/Services/ISchemeService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ColorBook.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace ColorBook.Services
 {
     interface ISchemeService
     {
+        ColorScheme GetEmptyScheme();
+        Task<ColorScheme[]> LibraryListAsync();
+        Task<bool> ExistsInLibraryAsync(Guid id);
+        Task LibrarySaveAsync(ColorScheme scheme);
+        Task LibraryRemoveAsync(ColorScheme scheme);
+
     }
 }
--- ColorBook/ColorBook/Services/SchemeService.cs
+++ ColorBook/ColorBook/Services/SchemeService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ColorBook.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,59 @@ namespace ColorBook.Services
 {
     public class SchemeService : ISchemeService
     {
+        public ColorScheme GetEmptyScheme()
+        {
+            return new ColorScheme
+            {
+                Id = Guid.NewGuid(),
+                Name = string.Empty,
+                Colors = new List<NamedColor>
+                {
+                    new NamedColor("#333333"),
+                },
+            };
+        }
+
+        public Task<bool> ExistsInLibraryAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ColorScheme[]> LibraryListAsync()
+        {
+            var list = new ColorScheme[]
+            {
+                new ColorScheme
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Test 001",
+                    Colors = new List<NamedColor>
+                    {
+                        new NamedColor("#0000ff"),
+                    },
+                },
+                new ColorScheme
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Test 002",
+                    Colors = new List<NamedColor>
+                    {
+                        new NamedColor("#4DEA26"),
+                        new NamedColor("#DDEEFF"),
+                    },
+                },
+            };
+            return await Task.FromResult(list);
+        }
+
+        public Task LibraryRemoveAsync(ColorScheme scheme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LibrarySaveAsync(ColorScheme scheme)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
